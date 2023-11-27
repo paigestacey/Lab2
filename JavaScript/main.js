e3fc38a656eb1b2b0e73d13cd88c4a4881f11fd8
Paige <gracepaigestacey@gmail.com>
2023-11-27 15:53:03 -0700

    footer date

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,4 +4,12 @@ var nav =
 document.getElementsByTagName("nav")[0];
 body.appendChild(button);
 
-</script>
+<footer>
+var today=new Date();  
+var h=today.getHours();  
+var m=today.getMinutes();  
+var s=today.getSeconds();  
+document.getElementById('txt').innerHTML=h+":"+m+":"+s; 
+</footer> 
+
+</script> 
