e12f99585050a11859e39e8c1ac5f3c155e0b0f1
Paige <gracepaigestacey@gmail.com>
2023-11-27 15:32:09 -0700

    ?? button

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,8 @@
+<script>
+const NewButton = document.createElement9 ('button');
+NewButton.textContent = 'Click me!';
+document.body.appendChild(newButton);
 
+newButton.addEventListener ('click', () {
+alert ( 'New button clicked!');
+</script>
