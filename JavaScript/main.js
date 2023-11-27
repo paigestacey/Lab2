6074d8afeec36911e2beb45466d8b9251c9c0a7e
Paige <gracepaigestacey@gmail.com>
2023-11-27 15:53:46 -0700

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,12 +4,12 @@ var nav =
 document.getElementsByTagName("nav")[0];
 body.appendChild(button);
 
-<footer>
+<myfooter>
 var today=new Date();  
 var h=today.getHours();  
 var m=today.getMinutes();  
 var s=today.getSeconds();  
 document.getElementById('txt').innerHTML=h+":"+m+":"+s; 
-</footer> 
+</myfooter> 
 
 </script> 
