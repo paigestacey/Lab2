4fc80e5eafd0f549f2c9ba71c87119ef01b75a51
Paige <gracepaigestacey@gmail.com>
2023-11-28 19:54:06 -0700

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,18 @@
 
 alert('Hello, World!');
 
+document.addEventListener("DOMContentLoaded", function () {
+    function formatDate(date) {
+        const options = { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit" };
+        return new Date(date).toLocaleString("en-US", options);
+    }
+
+    const lastUpdatedElement = document.getElementById("lastUpdated");
+
+    const currentDate = new Date();
+    lastUpdatedElement.textContent = formatDate(currentDate);
+});
+
 
 
 
