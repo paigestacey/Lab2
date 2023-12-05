f1f4deeb60bfbb09a29bc8cb7a72c0f0b1e38115
Paige <gracepaigestacey@gmail.com>
2023-12-04 20:55:35 -0700

    js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,33 @@
 
+//footer
+
+
+//alert button
+function displayTextOnClick() {
+
+  var displayElement = document.getElementById("displayText");
+
+  displayElement.textContent = "Welcome!";
+}
+
+var clickthis = document.getElementById("clickthis");
+
+clickthis.addEventListener("click". displayTextOnClick);
+
+//hover button
+
+
+
+//Incrementing button
+
+
+
+//if-else
+
+
+
+//loops
+
 
 
 
