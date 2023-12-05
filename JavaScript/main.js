dccd04e08a84664d11767927e4627458c8b1c558
Paige <gracepaigestacey@gmail.com>
2023-12-04 19:56:13 -0700

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -7,14 +7,11 @@ var date = `&copy; PaigeStacey ${year}.`;
 document.getElementsByTagName('footer')[0].innerHTML = date;
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const clickthis = document.getElementById('clickthis');
+ document.getElementById("clickthis").addEventListener("click", function() {
+ alert("Welcome!");
+ 
+ });
 
-  document.getElementById("clickthis").addEventListener("click", function () {
-   alert("Hello!");
-  });
-
-});
 
 
 
