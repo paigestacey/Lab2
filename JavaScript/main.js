cf4b73359d9433a17ecdd1adc6787fa0a57bdcaf
Paige <gracepaigestacey@gmail.com>
2023-12-04 20:19:35 -0700

    start over

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,15 +1,7 @@
 
-alert('Hello, World!');
 
-var year = new Date().getFullYear();
-var date = `&copy; PaigeStacey ${year}.`;
 
-document.getElementsByTagName('footer')[0].innerHTML = date;
 
-document.getElementById('clickthis').addEventListener('click', function() {
-  alert("Welcome!");
- 
- });
 
 
 
